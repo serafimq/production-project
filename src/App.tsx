ecf6fc610be6c21b49779effe8324363e06b16c0
yaozolin <yaozolin@phoenixit.ru>
2022-09-19 09:15:19 +0300

    added style structure and hook for toggle themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import {Suspense} from "react";
+import {Suspense, useContext} from "react";
 import {Routes, Route, Link} from "react-router-dom";
-import './index.scss';
+import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
