9e67f0c2e296eddc27b6874cfdb9664e2b03adf3
yaozolin <yaozolin@phoenixit.ru>
2022-11-07 09:03:42 +0300

    added Navbar + ui component AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,15 @@ import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
         </div>
     );
 };
