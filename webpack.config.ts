1cdf6d6088f6d43cf36725e5ca0e16ae433279ad
yaozolin <yaozolin@phoenixit.ru>
2022-11-13 12:09:38 +0300

    added NotFoundPage and component Loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => { // передаем env из package.json
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.tsx'), // стартовая точка нашего приложения
+        entry: path.resolve(__dirname, 'src', 'index.ts'), // стартовая точка нашего приложения
         build: path.resolve(__dirname, 'build'), // название папки, куда собирается билд
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'), // путь до папки src, для относительный импортов
