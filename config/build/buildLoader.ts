eda935d6ab5028df0555283c88eb3233a263169e
yaozolin <yaozolin@phoenixit.ru>
2023-04-01 01:55:58 +0300

    added new color style, added react refresh plugin

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,33 +1,17 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoader({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoader(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
     // loader для импорта svg
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/, // исключаем ноде модули
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        },
-                    ],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
 
     const cssLoader = buildCssLoader(isDev);
 
