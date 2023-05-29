4417dda9b4b3088e0796d837af6088309c290474
yaozolin <yaozolin@phoenixit.ru>
2023-05-29 04:07:17 +0300

    added circular dependency

--- src/entities/User/model/selectors/roleSelectors.ts
+++ src/entities/User/model/selectors/roleSelectors.ts
@@ -1,6 +1,6 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 import { createSelector } from '@reduxjs/toolkit';
-import { UserRole } from 'entities/User';
+import { UserRole } from '../consts/consts';
 
 export const getUserRoles = (state: StateSchema) => state.user.authData?.roles;
 export const isUserAdmin = createSelector(getUserRoles, (roles) => Boolean(roles?.includes(UserRole.ADMIN)));
