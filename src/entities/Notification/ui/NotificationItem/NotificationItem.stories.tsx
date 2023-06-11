b5f74b532178c1a4cb6c4519f89f0d90380c6e7d
yaozolin <yaozolin@phoenixit.ru>
2023-06-11 18:18:23 +0300

    fixed story book

--- src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { NotificationItem } from './NotificationItem';
 
 export default {
-    title: 'shared/NotificationItem',
+    title: 'entities/Notification/NotificationItem',
     component: NotificationItem,
     argTypes: {
         backgroundColor: { control: 'color' },
--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { AvatarDropdown } from './AvatarDropdown';
 
 export default {
-    title: 'shared/AvatarDropdown',
+    title: 'features/AvatarDropdown',
     component: AvatarDropdown,
     argTypes: {
         backgroundColor: { control: 'color' },
