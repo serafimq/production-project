1feb85570d49d3cccfd17e4315b69d53720cdd65
yaozolin <yaozolin@phoenixit.ru>
2023-06-19 18:04:22 +0300

    update storybook

--- src/features/articleRecommendationList/ui/ArticleRecommendationList/ArticleRecommendationList.stories.tsx
+++ src/features/articleRecommendationList/ui/ArticleRecommendationList/ArticleRecommendationList.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import withMock from 'storybook-addon-mock';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Article } from '@/entities/Article';
 import { ArticleRecommendationList } from './ArticleRecommendationList';
@@ -12,7 +11,6 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
-    decorators: [withMock],
 } as ComponentMeta<typeof ArticleRecommendationList>;
 
 const Template: ComponentStory<typeof ArticleRecommendationList> = (args) => <ArticleRecommendationList {...args} />;
